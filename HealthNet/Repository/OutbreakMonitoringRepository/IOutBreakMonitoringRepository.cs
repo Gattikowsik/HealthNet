b1448307f4c960462a542ab96b8f8e8d45736d68
Yogitha <yogithacheruku04@gmail.com>
2026-04-27 10:27:55 +0530

    API created for UpdateOutbreak

--- HealthNet/Repository/OutbreakMonitoringRepository/IOutBreakMonitoringRepository.cs
+++ HealthNet/Repository/OutbreakMonitoringRepository/IOutBreakMonitoringRepository.cs
@@ -1,11 +1,21 @@
 using System;
 using HealthNet.DTOs.OutbreakMonitoringDTO;
-
+using HealthNetDb.Entities;
 namespace HealthNet.Repository.OutbreakMonitoringRepository;
+
+public enum UpdateOutbreakResult
+{
+    NotFound,
+    NoChanges,
+    Updated,
+    Closed
+}
 
 public interface IOutBreakMonitoringRepository
 {
     Task<int> AddOutbreakAsync(CreateOutbreakRequestDto request);
     Task<bool> DuplicateOutbreakExitsAsync(CreateOutbreakRequestDto request);
-    Task<int> AddAuditLogAsync(int userId, string resource);
+    Task<int> AddAuditLogAsync(int userId, string actionName, string resource);
+    Task<Outbreak?> GetOutbreakByIdAsync(int outbreakId);
+    Task<UpdateOutbreakResult> UpdateOutbreakAsync(int outbreakId, UpdateOutbreakRequestDto request);
 }
